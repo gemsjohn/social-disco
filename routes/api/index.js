7a5c6179ea08222fea99c40a6bbaa3f5565fd3a4
JRM <johnrhmartin@gmail.com>
2022-05-01 19:25:07 -0400

    Updated server.js to fix the mongoose.connect issue. First user created

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-// const usersRoutes = require('./users-routes')
+const usersRoutes = require('./users-routes')
 
-// router.use('/users', usersRoutes);
+router.use('/users', usersRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,10 +10,12 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-disco', {
-//   useFindAndModify: false,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true
-// });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-disco', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
