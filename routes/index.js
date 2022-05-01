bc7a4e894b181c5babc9936dffd2a70d60ac068d
JRM <johnrhmartin@gmail.com>
2022-05-01 18:52:16 -0400

    Added a /api routeand the corresponding index.js router script

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
 
 // If invalid route, let user know with 404 error
 router.use((req, res) => {
