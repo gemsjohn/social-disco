cc794f23397474693542787aded7e34f2ce8d6ba
JRM <johnrhmartin@gmail.com>
2022-05-01 20:16:43 -0400

    updated Users.js model to icnlude thoughts which references the Thought model and friends which is a selfrefernece.

--- models/Users.js
+++ models/Users.js
@@ -13,7 +13,37 @@ const UsersSchema = new Schema ({
         unique: true,
         required: 'Email is Required',
         match: [/.+@.+\..+/]
-    }
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thoughts'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Users'
+        }
+    ]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+}
+);
+
+UsersSchema.virtual('thoughtsCount').get(function() {
+    return this.thoughts.reduce(
+      (total, thought) => total + thought.reactions.length + 1,
+      0
+    );
+});
+UsersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
 });
 
 const Users = model('Users', UsersSchema);
