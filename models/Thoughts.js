21730bc06c65171244b0a38d876d3111ea4ae12a
JRM <johnrhmartin@gmail.com>
2022-05-01 20:03:04 -0400

    Updated Throughts.js model to include Reaction Schema

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -1,6 +1,35 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema (
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+          getters: true
+        }
+    }
+);
+
 const ThoughtsSchema = new Schema ({
     thoughtText: {
         type: String,
@@ -13,8 +42,25 @@ const ThoughtsSchema = new Schema ({
         type: Date,
         default: Date.now,
         get: createdAtVal => dateFormat(createdAtVal)
-    }
-});
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+},
+{
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+}
+);
+
+ThoughtsSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
 
 const Thoughts = model('Thoughts', ThoughtsSchema);
 
